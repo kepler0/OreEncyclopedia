0c001ff3d1fe3445b3f8bde3acc008a5d66dff42
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-08-31 21:23:07 -0600

    Create OreCyclopedia.java
    From my iPad!

---
+++ common/k2b6s9j/OreEncyclopia/OreCyclopedia.java
@@ -0,0 +1,32 @@
+package k2b6s9j.OreEncyclopedia;
+
+import java.io.IOException;
+import java.util.List;
+
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.ModContainer;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "OreEncyclopedia", name = "OreEncylopedia", version = "1.0")
+public class OreEncyclopedia {
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+
+	}
+
+	@EventHandler
+	public void Init(FMLInitializationEvent event) {
+
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	}
+}
