6c69fde5366af57e6a21b2e794ad2aa0e5a936ad
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-08-31 23:23:12 -0600

    Log mod info and a config option for it

--- common/k2b6s9j/OreEncyclopedia/OreEncyclopedia.java
+++ common/k2b6s9j/OreEncyclopedia/OreEncyclopedia.java
@@ -1,7 +1,10 @@
 package k2b6s9j.OreEncyclopedia;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import net.minecraftforge.common.Configuration;
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -11,16 +14,37 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = "OreEncyclopedia", name = "OreEncylopedia", version = "1.0")
 public class OreEncyclopedia {
 	
+	//Logger
 	public static final Logger oeLog = Logger.getLogger("OreEncyclopedia");
+	
+	//Configuration Booleans
+	public static Boolean logInfo;
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-
+		Configuration cfg = new Configuration(event.getSuggestedConfigurationFile());
+        try
+        {
+        	logInfo = cfg.get("Features", "logInfo", true, "Should post mod name, copyright, and website in log.").getBoolean(true);
+        }
+        catch (Exception e)
+        {
+            FMLLog.log(Level.SEVERE, e, "OreDictionary had a problem loading it's configuration");
+        }
+        finally
+        {
+            if (cfg.hasChanged())
+                cfg.save();
+        }
 	}
 
 	@EventHandler
 	public void Init(FMLInitializationEvent event) {
-
+		if (logInfo) {
+			oeLog.info("OreEncyclopedia");
+			oeLog.info("Copyright Kepler Sticka-Jones 2013");
+			oeLog.info("http://k2b6s9j.com/projects/minecraft/oreencyclopedia");
+		}
 	}
 
 	@EventHandler
