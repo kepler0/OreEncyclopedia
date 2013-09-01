64c37ca05cec76ccb0cbb9450047a474cd4b1fec
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-08-31 23:14:13 -0600

    Add a distinct logger

--- common/k2b6s9j/OreEncyclopedia/OreEncyclopedia.java
+++ common/k2b6s9j/OreEncyclopedia/OreEncyclopedia.java
@@ -1,19 +1,17 @@
 package k2b6s9j.OreEncyclopedia;
 
-import java.io.IOException;
-import java.util.List;
+import java.util.logging.Logger;
 
-import cpw.mods.fml.common.FMLLog;
-import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "OreEncyclopedia", name = "OreEncylopedia", version = "1.0")
 public class OreEncyclopedia {
+	
+	public static final Logger oeLog = Logger.getLogger("OreEncyclopedia");
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
