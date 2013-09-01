7553ca48ff2317c5e01023ebc3d68dafff2ec94e
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-09-01 13:30:57 -0600

    Some basic command usage

--- common/k2b6s9j/OreEncyclopedia/command/OECommand.java
+++ common/k2b6s9j/OreEncyclopedia/command/OECommand.java
@@ -3,9 +3,9 @@ package k2b6s9j.OreEncyclopedia.command;
 import java.util.ArrayList;
 import java.util.List;
 
-import k2b6s9j.OreEncyclopedia.OreEncyclopedia;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.command.WrongUsageException;
 import net.minecraft.util.ChatMessageComponent;
 
 public class OECommand implements ICommand
@@ -25,10 +25,9 @@ public class OECommand implements ICommand
   }
 
   @Override
-  public String getCommandUsage(ICommandSender icommandsender)
-  {
-    return "oreencyclopedia <text>";
-  }
+	public String getCommandUsage(ICommandSender sender) {
+		return "/" + this.getCommandName() + " <help|list|exchange>";
+	}
 
   @Override
   public List getCommandAliases()
@@ -37,9 +36,26 @@ public class OECommand implements ICommand
   }
 
   @Override
-  public void processCommand(ICommandSender icommandsender, String[] astring)
+  public void processCommand(ICommandSender sender, String[] arguments)
   {
-	  OreEncyclopedia.oeLog.info("Command called!");
+	  if (arguments.length <= 0)
+			throw new WrongUsageException("Type '" + this.getCommandUsage(sender) + "' for help.");
+
+		if (arguments[0].matches("help")) {
+			sender.sendChatToPlayer(ChatMessageComponent.func_111066_d("Format: '" + this.getCommandName() + " <command> <arguments>'"));
+			sender.sendChatToPlayer(ChatMessageComponent.func_111066_d("Available commands:"));
+			sender.sendChatToPlayer(ChatMessageComponent.func_111066_d("- list : Lists all of the items belonging to an entry."));
+			sender.sendChatToPlayer(ChatMessageComponent.func_111066_d("- exchange : Exchange the item being held for another in the same entry."));
+			return;
+		} else if (arguments[0].matches("list")) {
+			sender.sendChatToPlayer(ChatMessageComponent.func_111066_d("Not yet implemented"));
+			return;
+		} else if (arguments[0].matches("exchange")) {
+			sender.sendChatToPlayer(ChatMessageComponent.func_111066_d("Not yet implemented"));
+			return;
+		}
+
+		throw new WrongUsageException(this.getCommandUsage(sender));
   }
 
   @Override
