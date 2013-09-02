3c28776aad5520931a69fc33d15122f2a46a2b0d
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-09-02 16:44:51 -0600

    oe list all now shows page 1

--- common/k2b6s9j/OreEncyclopedia/command/OECommand.java
+++ common/k2b6s9j/OreEncyclopedia/command/OECommand.java
@@ -77,7 +77,7 @@ public class OECommand extends CommandBase
 		  int perPage = 7;
 		  int pages = (int) Math.ceil(size / (float) perPage);
 			
-		  int page = arguments.length == 2 ? 2 : parseIntBounded(sender, arguments[2], 1, pages) - 1;
+		  int page = arguments.length == 2 ? 0 : parseIntBounded(sender, arguments[2], 1, pages) - 1;
 		  int min = Math.min(page * perPage, size);
 
 		  sender.sendChatToPlayer(ChatMessageComponent.func_111082_b("commands.help.header", new Object[] {Integer.valueOf(page + 1), Integer.valueOf(pages)}).func_111059_a(EnumChatFormatting.DARK_GREEN));
